49ae869e80a085ea8814f9b1b3ae2a44a947409e
汝秋岚 <ruqiulan@outlook.com>
2025-07-06 17:33:18 +0800

    Add files via upload

---
+++ src/main/java/com/ruqiulan/lanmod/LanModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.ruqiulan.lanmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LanModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
